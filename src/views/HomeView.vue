b7f121436c1de6c8e1a7cfa6303a649320b8e140
Julian Anaya <juliananaya531@gmail.com>
2022-06-01 10:29:06 -0700

    added images to frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,30 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "hello and welcome to! ",
+      message: "Welcome to Vues",
       samp: "sample",
       items: [{ message: "Foo" }, { message: "Bar" }, { message: "hello there" }],
-      products: [
-        { id: 1, name: "Product 1", price: 10 },
-        { id: 2, name: "Product 2", price: 230 },
-      ],
+      products: [],
     };
   },
-  created: function () {},
+  created: function () {
+    this.indexProducts();
+  },
   methods: {
     helloWorld: function () {
       console.log("hello");
     },
     wordPlay: function () {
       console.log(this.message + this.samp);
+    },
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products.json").then((response) => {
+        this.products = response.data;
+        console.log("All products", response.data);
+      });
     },
   },
 };
@@ -26,15 +33,16 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h3>{{ samp }}</h3>
+    <!-- <h3>{{ samp }}</h3>
     <p>helloWorld</p>
     <button v-on:click="helloWorld">hi</button>
     <button v-on:click="wordPlay">words</button>
     <li v-for="item in items" :key="item">
       {{ item.message }}
-    </li>
+    </li> -->
     <div v-for="product in products" v-bind:key="product.id">
-      <h2>Name: {{ product.name }}</h2>
+      <h1>Name: {{ product.name }}</h1>
+      <img v-bind:src="product.image_url" v-bind:alt="product.title" />
       <p>Price: {{ product.price }}</p>
     </div>
   </div>
