722a445a8289d3f954a9e3874d8d9d02158f6e89
Julian Anaya <juliananaya531@gmail.com>
2022-06-01 09:15:39 -0700

    made simple frontend and played with it

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "hello and welcome to! ",
+      samp: "sample",
+      items: [{ message: "Foo" }, { message: "Bar" }, { message: "hello there" }],
+    };
+  },
+  created: function () {},
+  methods: {
+    helloWorld: function () {
+      console.log("hello");
+    },
+    wordPlay: function () {
+      console.log(this.message + this.samp);
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h3>{{ samp }}</h3>
+    <p>helloWorld</p>
+    <button v-on:click="helloWorld">hi</button>
+    <button v-on:click="wordPlay">words</button>
+    <li v-for="item in items" :key="item">
+      {{ item.message }}
+    </li>
+  </div>
+</template>
+
+<style></style>
